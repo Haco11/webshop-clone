eb95e2a2257fa0a22bc6f469bd392ebae0a65132
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-12 23:40:11 +0200

    Added first section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import styles from "./page.module.scss";
+import im1 from "../assets/img1.png";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.container}>
-      <section></section>
+      <section className={styles.wrap}>
+        <div className={styles.main}>
+          <Image src={im1} alt="aa" />
+        </div>
+      </section>
       <section></section>
     </main>
   );
