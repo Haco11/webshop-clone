9e39d71fcf10d9c34ca3e961f43031e4443f38f5
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-16 01:53:35 +0200

    Adding footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,50 @@
 import React from "react";
 import style from "./Footer.module.scss";
-
+import im12 from "../../assets/img12.svg";
+import Image from "next/image";
+import {
+  FaLinkedin,
+  FaTwitter,
+  FaFacebook,
+  FaYoutube,
+  FaLinkedinIn,
+  FaInstagram,
+} from "react-icons/fa";
 const Footer = () => {
-  return <div className={style.footer}>Footer</div>;
+  return (
+    <div className={style.footer}>
+      <div>
+        <div>
+          <h2>
+            <p>THE</p>
+            <p>SHOW</p>
+            <p>MUST GO ON</p>
+          </h2>
+        </div>
+        <div>
+          <div>
+            <p>Wearable</p>
+            <p>Collectibles</p>
+          </div>
+          <div>
+            <p> Customer support</p>
+            <p>Terms & Conditions</p>
+          </div>
+        </div>
+        <div>
+          <FaInstagram />
+          <FaTwitter />
+          <FaFacebook />
+          <FaYoutube />
+          <FaLinkedin />
+        </div>
+        <a href="">
+          <Image src={im12} />
+        </a>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Footer;
