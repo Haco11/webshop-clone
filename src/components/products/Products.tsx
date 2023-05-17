95ec2e55ed6e2302baf18cc4c43a6e881f2cdc25
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-17 22:53:00 +0200

    Added all products to data

---
+++ src/components/products/Products.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./Products.module.scss";
+
+const Products = () => {
+  return <div>Products</div>;
+};
+
+export default Products;
--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,46 @@
+import im6 from "./assets/img6.jpg";
+import im7 from "./assets/img7.jpg";
+import im8 from "./assets/img8.jpg";
+import im9 from "./assets/img9.jpg";
+import im10 from "./assets/img10.jpg";
+import im11 from "./assets/img11.jpg";
+
 export const data = [
   {
     id: 1,
-    title: "Test",
-    price: 100,
-    image_url: "url",
+    title: "Ghost Squadron Sweatshirt",
+    price: 999,
+    image_url: im6,
+  },
+  {
+    id: 2,
+    title: "Koenigsegg Sweatshirt",
+    price: 999,
+    image_url: im7,
+  },
+  {
+    id: 3,
+    title: "Ghost Sweatshirt",
+    price: 999,
+    image_url: im8,
+  },
+  {
+    id: 4,
+    title: "Black Cap",
+    price: 355,
+    image_url: im9,
+  },
+  {
+    id: 5,
+    title: "Koenigsegg Keyring",
+    price: 359,
+    image_url: im10,
+  },
+  {
+    id: 6,
+    title: "Grey Cap",
+    price: 355,
+    image_url: im11,
   },
 ];
 
