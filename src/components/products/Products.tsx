601d408257399b6eacbfebc7f395d7b8260ffa2f
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-17 22:59:51 +0200

    Making Products component

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,8 +1,24 @@
 import React from "react";
-import style from "./Products.module.scss";
+import styles from "./Products.module.scss";
+import Image from "next/image";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <div className={styles.product__container}>
+        <div className={styles.products__title}>
+          <h2>HOODIES AND SWEATSHIRTS</h2>
+        </div>
+        <div className={styles.products}>
+          <div className={styles.product}>
+            <Image src="aaa" alt="product" />
+            <h4>Ghost Squadron Sweatshirt</h4>
+            <p>999 SEK</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Products;
