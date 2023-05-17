a98e436f34d12a63e16ebc5addeb35b9bfdf0305
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-17 23:48:22 +0200

    Added link to Products and changed style

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import styles from "./Products.module.scss";
 import Image from "next/image";
 import { Product } from "@/type/Product";
+import Link from "next/link";
 interface Props {
   product: Product;
 }
 const Products: React.FC<Props> = ({ product }) => {
   return (
-    <div className={styles.product}>
+    <Link href={`/Product/${product.id}`} className={styles.product}>
       <Image src={product.image_url} alt="product" />
       <h4>{product.title}</h4>
       <p>{product.price} SEK</p>
-    </div>
+    </Link>
   );
 };
 
