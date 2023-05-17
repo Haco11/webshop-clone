486eedabdfc37008106eb9e457d3620bfb109a05
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-18 00:17:44 +0200

    Page for single product

--- src/app/Product/[id]/page.tsx
+++ src/app/Product/[id]/page.tsx
@@ -1,7 +1,27 @@
+"use client";
 import React from "react";
+import { usePathname } from "next/navigation";
+import { Product } from "@/type/Product";
+import { data } from "../../../data";
+import Image from "next/image";
 
 const page = () => {
-  return <div>page</div>;
+  const router = usePathname();
+  const productId = router.split("/").pop();
+
+  const product: Product | undefined = data.find(
+    (item) => item.id === Number(productId)
+  );
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+  return (
+    <div>
+      <h2>{product.title}</h2>
+      <p>{product.price} SEK</p>
+      <Image src={product.image_url} alt="product" />
+    </div>
+  );
 };
 
 export default page;
