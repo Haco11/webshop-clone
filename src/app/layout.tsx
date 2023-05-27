1487aed8eb7dc6d9ec1bb5ae3937c6b16348202e
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-05-27 23:02:51 +0200

    Fixed mobile nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,12 @@ import Footer from "@/components/footer/Footer";
 import CartProvider from "@/context/cartContext";
 import CartConent from "@/components/cart/CartContent";
 import "./globals.scss";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Koenigsegg",
+  description: "A shoping clone ",
+};
 
 export default function RootLayout({
   children,
